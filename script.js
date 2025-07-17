c90b495f05af5ac1b91f2994757b39ec5d45a4f9
cevo997 <146278229+cevo997@users.noreply.github.com>
2025-07-17 17:37:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-
-Stefan Barać <stefanbarac@gmail.com>
-17:35 (пре 0 минута)
-коме ја
-
 const API_URL = "https://sheetdb.io/api/v1/ywvbhlm9ikdui";
 const agenti = ["SAŠKA", "CECA", "LJILJA", "RUŽA", "SEKA", "SANDRA", "NIKOLA", "ANĐELA", "ATINA", "VIŠNJA", "MILA", "BUDA", "NATAŠA"];
 const vremeTermina = ["09:00", "09:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30", "18:00", "18:30", "19:00", "19:30"];
